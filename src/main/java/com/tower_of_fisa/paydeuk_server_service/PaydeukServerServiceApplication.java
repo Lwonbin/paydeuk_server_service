06aa125b7aed4fe5986acb51235491e5d36e6568
Cha Seung Hoon <58865827+eratchacha@users.noreply.github.com>
2025-05-05 22:10:51 +0900

    Adjusting Sonar Qube (#1)
    * first commit
    
    * add sonar qube yaml file
    
    * edit sonar qube target: dev to all
    
    * change branch range

---
+++ src/main/java/com/tower_of_fisa/paydeuk_server_service/PaydeukServerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tower_of_fisa.paydeuk_server_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaydeukServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaydeukServerServiceApplication.class, args);
+	}
+
+}
