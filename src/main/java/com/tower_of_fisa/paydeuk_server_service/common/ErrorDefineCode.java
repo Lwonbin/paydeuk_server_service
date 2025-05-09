9ef4efeb87976f55a7707962e2d4089812048c8d
icebear0111 <wltjq0901@naver.com>
2025-05-09 17:42:16 +0900

    chore(error-code): 커스텀 에러 코드 [#19]
    커스텀 에러 코드 변경
    - 에러 코드 명 큐칙에 맞게 VERIFICATION의 앞 세글자, VER_XX로 수정

--- src/main/java/com/tower_of_fisa/paydeuk_server_service/common/ErrorDefineCode.java
+++ src/main/java/com/tower_of_fisa/paydeuk_server_service/common/ErrorDefineCode.java
@@ -13,10 +13,10 @@ public enum ErrorDefineCode {
   AUTH_NOT_FOUND_EMAIL("ERR_04", "해당 이메일을 찾을 수 없습니다."),
   AUTHORIZATION_FAIL("ERR_05", "해당 권한이 없습니다."),
   AUTHENTICATE_FAIL("ERR_06", "권한 인증에 실패했습니다."),
-  VERIFICATION_TOKEN_NULL("ERR_07", "토큰 발급 응답이 null입니다."),
-  VERIFICATION_RESPONSE_NULL("ERR_08", "인증 응답의 response 필드가 null입니다."),
-  VERIFICATION_ACCESS_TOKEN_NULL("ERR_09", "인증 응답의 access_token 필드가 null입니다."),
-  VERIFICATION_RESULT_NULL("ERR_10", "인증 결과 조회 응답이 null입니다."),
+  VERIFICATION_TOKEN_NULL("VER_01", "토큰 발급 응답이 null입니다."),
+  VERIFICATION_RESPONSE_NULL("VER_02", "인증 응답의 response 필드가 null입니다."),
+  VERIFICATION_ACCESS_TOKEN_NULL("VER_03", "인증 응답의 access_token 필드가 null입니다."),
+  VERIFICATION_RESULT_NULL("VER_04", "인증 결과 조회 응답이 null입니다."),
   ;
 
   private final String code;
