2d761c60bec6f35fe932ec52e11b8e49cb7c5a2b
Cha Seung Hoon <58865827+eratchacha@users.noreply.github.com>
2025-05-12 17:22:03 +0900

    Setting: 프로젝트 추가 세팅 적용 (#39)
    * fix(build.yaml): Github Actions application-secret.yml 적용 [#31]
    application-secret.yml을 Github Actions에서도 적용합니다.
    
    * fix(github actions): .yaml 파일 확장자 변경 [#31]
    github actions 파일에서 application-secret.yaml파일 을 생성하여 .yml로 변경:
    
    * setting(git hooks): build failed push block hook [#31]
    build failed 시 Push block하는 git hooks 생성 및 설정 방법 추가
    
    * setting(workflows): after merge build check [#31]
    dev로 merge 실행 시에 병합된 dev 브랜치 BUIld여부 확인하고 실패 시 merge를 block
    
    * Test for Merge Queue
    
    Blocking Failed Build after merge

---
+++ hooks/README.md
@@ -0,0 +1,4 @@
+```shell
+cp hooks/pre-push.sh .git/hooks/pre-push
+chmod +x .git/hooks/pre-push
+```
--- src/main/java/com/tower_of_fisa/paydeuk_server_service/PaydeukServerServiceApplication.java
+++ src/main/java/com/tower_of_fisa/paydeuk_server_service/PaydeukServerServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class PaydeukServerServiceApplication {
+public class PaydeukServerServiceApplicati {
 
   public static void main(String[] args) {
     SpringApplication.run(PaydeukServerServiceApplication.class, args);
