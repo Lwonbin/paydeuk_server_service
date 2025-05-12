e467a07bbf67fd867e80a9fcbb229a619c391aa5
Cha Seung Hoon <58865827+eratchacha@users.noreply.github.com>
2025-05-12 18:17:06 +0900

    Update PaydeukServerServiceApplication.java

--- src/main/java/com/tower_of_fisa/paydeuk_server_service/PaydeukServerServiceApplication.java
+++ src/main/java/com/tower_of_fisa/paydeuk_server_service/PaydeukServerServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class PaydeukServerServiceApplication {
+public class PaydeukServerServiceApplicatio {
 
   public static void main(String[] args) {
     SpringApplication.run(PaydeukServerServiceApplication.class, args);
