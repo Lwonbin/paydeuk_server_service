198a52b33716a7b381654548ae2d00d83fcfcc6b
Lwonbin <lwb9036@gmail.com>
2025-05-13 16:45:26 +0900

    feat(swagger): Swagger Schema설정[#65] -Swagger에서 예시로 보여주기 위해 DTO에서  Schema를 설정

--- src/main/java/com/tower_of_fisa/paydeuk_server_service/user/dto/UpdateProfileRequest.java
+++ src/main/java/com/tower_of_fisa/paydeuk_server_service/user/dto/UpdateProfileRequest.java
@@ -1,5 +1,6 @@
 package com.tower_of_fisa.paydeuk_server_service.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -8,8 +9,10 @@ import lombok.Getter;
 public class UpdateProfileRequest {
 
     @Email
+    @Schema(description = "이메일", example = "abc1234@naver.com")
     private String email;
 
     @Size(max = 30)
+    @Schema(description = "주소", example = "서울 마포구 월드컵북로 434 상암 IT Tower")
     private String address;
 }
