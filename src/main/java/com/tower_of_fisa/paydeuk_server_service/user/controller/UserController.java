898becb07aa8ab1d1facc571d9baba8ab5130d4a
Lwonbin <lwb9036@gmail.com>
2025-05-14 17:55:37 +0900

    feat(user-info): user정보 반환[#72] - 유저 정보 반환 api 작성 - 이름, 이메일, 주소, 생년월일

--- src/main/java/com/tower_of_fisa/paydeuk_server_service/user/controller/UserController.java
+++ src/main/java/com/tower_of_fisa/paydeuk_server_service/user/controller/UserController.java
@@ -6,6 +6,7 @@ import com.tower_of_fisa.paydeuk_server_service.common.response.swagger_response
 import com.tower_of_fisa.paydeuk_server_service.config.security.CustomUserDetails;
 import com.tower_of_fisa.paydeuk_server_service.user.dto.UpdateAddressRequest;
 import com.tower_of_fisa.paydeuk_server_service.user.dto.UpdateEmailRequest;
+import com.tower_of_fisa.paydeuk_server_service.user.dto.UserInfoResponse;
 import com.tower_of_fisa.paydeuk_server_service.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -82,5 +83,21 @@ public class UserController {
   public CommonResponse<EmptyResponse> checkUserExists(@PathVariable Long userId) {
     userService.checkUserExists(userId);
     return new CommonResponse<>(true, HttpStatus.OK, "사용자가 존재합니다.", new EmptyResponse());
+  }
+
+  @GetMapping("/profile")
+  @Operation(summary = "USER_04 : 내 정보 조회", description = "로그인한 사용자의 정보를 조회합니다.")
+  @ApiResponses(
+          value = {
+                  @ApiResponse(responseCode = "200", description = "내 정보 조회 성공"),
+                  @ApiResponse(
+                          responseCode = "404",
+                          description = "사용자를 찾을 수 없음",
+                          content = @Content(examples = @ExampleObject(value = SwaggerResponseExample.USER_404)))
+          }
+  )
+  public CommonResponse<UserInfoResponse> getUserProfile(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    UserInfoResponse response = userService.getUserInfo(userDetails.getId());
+    return new CommonResponse<>(true, HttpStatus.OK, "내 정보 조회에 성공했습니다.", response);
   }
 }
---
+++ src/main/java/com/tower_of_fisa/paydeuk_server_service/user/dto/UserInfoResponse.java
@@ -0,0 +1,13 @@
+package com.tower_of_fisa.paydeuk_server_service.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserInfoResponse {
+    private String name;
+    private String birth;
+    private String phoneNumber;
+    private String email;
+}
