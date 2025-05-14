53498b7050139656b2abf13062ac8157ceea80e1
Lwonbin <lwb9036@gmail.com>
2025-05-14 10:19:46 +0900

    feat(profile): 주소, 이메일 API 분리[#67] - 하나로 작성되어있던 profile 변경을 email, address 변경으로 분리

---
+++ src/main/java/com/tower_of_fisa/paydeuk_server_service/user/dto/UpdateEmailRequest.java
@@ -0,0 +1,13 @@
+package com.tower_of_fisa.paydeuk_server_service.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+
+@Getter
+public class UpdateEmailRequest {
+
+  @Email
+  @Schema(description = "이메일", example = "abc1234@naver.com")
+  private String email;
+}
