5dd4aa2303b557f873e5f7790f555d150b9c0137
eratchacha <papa021326@naver.com>
2025-05-14 15:04:39 +0900

    merge(UserCardService): 병합 시 Build Failed 해결 [#60] dev 브랜치 병합 시 발생한 build failed 요소 수정 UserCardService의 UserRepository import문 auth에서 user로 수정

--- src/main/java/com/tower_of_fisa/paydeuk_server_service/user_card/service/UserCardService.java
+++ src/main/java/com/tower_of_fisa/paydeuk_server_service/user_card/service/UserCardService.java
@@ -1,7 +1,6 @@
 package com.tower_of_fisa.paydeuk_server_service.user_card.service;
 
 import com.tower_of_fisa.paydeuk_server_service.admin.repository.PaymentRepository;
-import com.tower_of_fisa.paydeuk_server_service.auth.repository.UserRepository;
 import com.tower_of_fisa.paydeuk_server_service.card.repository.CardRepository;
 import com.tower_of_fisa.paydeuk_server_service.common.ErrorDefineCode;
 import com.tower_of_fisa.paydeuk_server_service.config.exception.custom.exception.NoSuchElementFoundException404;
@@ -10,6 +9,7 @@ import com.tower_of_fisa.paydeuk_server_service.domain.entity.CardBenefit;
 import com.tower_of_fisa.paydeuk_server_service.domain.entity.Payment;
 import com.tower_of_fisa.paydeuk_server_service.domain.entity.User;
 import com.tower_of_fisa.paydeuk_server_service.domain.entity.UserCard;
+import com.tower_of_fisa.paydeuk_server_service.user.repository.UserRepository;
 import com.tower_of_fisa.paydeuk_server_service.user_card.dto.*;
 import com.tower_of_fisa.paydeuk_server_service.user_card.repository.UserCardRepository;
 import java.util.List;
