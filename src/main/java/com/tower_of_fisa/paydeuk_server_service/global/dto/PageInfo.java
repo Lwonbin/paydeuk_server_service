c9c87cd580622af985f0f4e4845e09d1139b047c
sungjoon <dkdldrha@naver.com>
2025-05-16 17:40:42 +0900

    feat(swagger): 페이지네이션 dto 스웨거 추가 [#91] - 스웨거에 페이지네이션 dto를 추가한다.

--- src/main/java/com/tower_of_fisa/paydeuk_server_service/global/dto/PageInfo.java
+++ src/main/java/com/tower_of_fisa/paydeuk_server_service/global/dto/PageInfo.java
@@ -1,11 +1,17 @@
 package com.tower_of_fisa.paydeuk_server_service.global.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "페이지 정보")
 public class PageInfo {
-  private int pageNumber;
-  private int pageSize;
+
+  @Schema(description = "현재 페이지 번호 (1부터 시작)" , example = "1")
+  private int page;
+
+  @Schema(description = "페이지 크기", example = "5")
+  private int size;
 }
