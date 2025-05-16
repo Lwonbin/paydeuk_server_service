9f555106b9eb81beac339fa9407092434abcf96e
sungjoon <dkdldrha@naver.com>
2025-05-16 11:02:41 +0900

    feat(Pagination): 가맹점 목록 조회 페이지네이션 [#82] - pagination 위한 공통 dto

---
+++ src/main/java/com/tower_of_fisa/paydeuk_server_service/global/dto/CustomPageResDto.java
@@ -0,0 +1,28 @@
+package com.tower_of_fisa.paydeuk_server_service.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class CustomPageResDto<T> {
+    private List<T> content;
+    private PageInfo pageable;
+    private int totalPages;
+    private long totalElements;
+
+    public static <T> CustomPageResDto<T> fromPage(Page<T> page) {
+        return new CustomPageResDto<>(
+                page.getContent(),
+                new PageInfo(
+                        page.getNumber() + 1,  //1부터 시작하는 페이지 변환
+                        page.getSize()
+                ),
+                page.getTotalPages(),
+                page.getTotalElements()
+        );
+    }
+}
