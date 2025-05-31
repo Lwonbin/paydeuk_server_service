89289f7885ffca38916149530b61e95871be2abb
icebear0111 <wltjq0901@naver.com>
2025-05-31 16:14:50 +0900

    feat(dto): 카드 상세 정보 DTO 클래스 생성 [#124]
    카드 상세 정보 DTO 클래스 생성
    - 카드 상세 정보 응답

---
+++ src/main/java/com/tower_of_fisa/paydeuk_server_service/user_card/dto/CardDetailResponse.java
@@ -0,0 +1,32 @@
+package com.tower_of_fisa.paydeuk_server_service.user_card.dto;
+
+import com.tower_of_fisa.paydeuk_server_service.domain.enums.CardCompany;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@Schema(description = "카드 상세 정보 응답")
+public class CardDetailResponse {
+
+  @Schema(description = "카드 이름", example = "카드의정석")
+  private final String cardName;
+
+  @Schema(description = "카드 이미지 URL", example = "https://example.com/card.png")
+  private final String imageUrl;
+
+  @Schema(description = "카드 회사", example = "WOORI")
+  private final CardCompany cardCompany;
+
+  @Schema(description = "연회비", example = "10,000원")
+  private final Long annualFee;
+
+  @Schema(description = "최소 사용금액", example = "300,000원")
+  private final String minSpending;
+
+  @Schema(description = "카드 혜택 목록")
+  private final List<BenefitResponse> benefits;
+}
