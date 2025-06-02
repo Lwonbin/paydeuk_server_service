9da1e1fa1fbeec63f290c4e32a0284701cd201a9
icebear0111 <wltjq0901@naver.com>
2025-06-02 10:22:18 +0900

    feat(payment-repository): 쿼리문 수정 [#131]
    쿼리문 수정
    - JPQL의 ORDER BY 절을 사용하면 서비스 메서드에서 sort를 하려고 해도 무시되기 때문에 ORDER BY 삭제

--- src/main/java/com/tower_of_fisa/paydeuk_server_service/admin/repository/PaymentRepository.java
+++ src/main/java/com/tower_of_fisa/paydeuk_server_service/admin/repository/PaymentRepository.java
@@ -67,8 +67,7 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
           + "JOIN FETCH p.merchant m "
           + "JOIN FETCH p.cardBenefit cb "
           + "JOIN FETCH cb.benefit b "
-          + "WHERE uc.user.id = :userId "
-          + "ORDER BY p.createdAt DESC")
+          + "WHERE uc.user.id = :userId ")
   Page<Payment> findPaymentHistoryByUserId(@Param("userId") Long userId, Pageable pageable);
 
   @Query("SELECT COUNT(p) FROM Payment p WHERE p.paymentSuccess = true AND p.createdAt < :before")
